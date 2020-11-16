5f392ccbec81ed48d8001393481ca0b8c83dccbc
edward79519 <edward79519@gmail.com>
2020-11-16 16:28:34 +0800

    change database setting

--- crmsystem/settings.py
+++ crmsystem/settings.py
@@ -72,11 +72,23 @@ WSGI_APPLICATION = 'crmsystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'it_crm',
+        'USER': 'it_crm',
+        'PASSWORD': 'it_password',
+        'HOST': '192.168.11.171',
+        'PORT': '3306',
     }
 }
 
